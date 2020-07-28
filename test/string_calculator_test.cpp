9bc019c302ccaeed0779e778ba9afc1903431142
Bartolome Jimenez Vera <bartolomehjvera@gmail.com>
2020-07-28 21:25:10 +0200

    Add basic string calculator single number operations

--- test/string_calculator_test.cpp
+++ test/string_calculator_test.cpp
@@ -1,7 +1,50 @@
+#include <string>
+
 #include "gtest/gtest.h"
 
-// This is just a placeholder to begin with. To be sure everything builds
-TEST(TestDummyClass, Should_Pass)
+class StringCalculator
 {
-    EXPECT_EQ(1, 1);
+public:
+    StringCalculator() = default;
+
+    int add(std::string numbers)
+    {
+        if (numbers.empty())
+        {
+            return 0;
+        }
+
+        return std::stoi(numbers);
+    }
+};
+
+class StringCalculatorTestFixture : public ::testing::Test
+{
+protected:
+    StringCalculator sc{};
+};
+
+TEST_F(StringCalculatorTestFixture, Should_ReturnZeroWhenEmptyString)
+{
+    int result = sc.add("");
+    ASSERT_EQ(0, result);
 }
+
+class StringCalculatorTestFixtureOneArgument : public ::testing::TestWithParam<std::string>
+{
+protected:
+    StringCalculator sc{};
+};
+
+TEST_P(StringCalculatorTestFixtureOneArgument, When_NoComma_Should_ReturnSameValueAsInput)
+{
+    std::string expected = GetParam();
+    int result = sc.add(expected);
+    ASSERT_EQ(std::stoi(expected), result);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    StringCalculatorSingleArgumentTests,
+    StringCalculatorTestFixtureOneArgument,
+    ::testing::Values("1", "105", "800"));
+
